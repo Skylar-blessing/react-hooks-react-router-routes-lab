bc76e707ee800dd53b6881c5f3cf5333a5a3bc44
Skylar <skylarblessings5@gmail.com>
2023-05-03 12:19:34 +0300

    task

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,21 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { actors, movies } from "../data";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+    <BrowserRouter>
+    <div>
+      <NavBar />
+      <route exact path="/" component={Home}/>
+      <route path="/movies" component={Movies}/>
+      <route path="/directors" component={Directors}/>
+      <route path="/actors" component={Actors}/>
+
+    </div>
+    </BrowserRouter>
+  );
 
 export default App;
